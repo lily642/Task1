d0096f781c512d1f23f77502735c3f7bc088acd4
lily642 <leilaashraf72@gmail.com>
2024-08-17 23:15:06 +0300

    task21

---
+++ day 4.dart/task21.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+
+void main() {
+  Book TheHumanMind = Book();
+  TheHumanMind.title = 'MIND';
+  TheHumanMind.author = 'JohnR.Andreson';
+  TheHumanMind.numberOfpages = 576;
+  print(TheHumanMind.title);
+  print(TheHumanMind.author);
+  print(TheHumanMind.numberOfpages);
+}
+
+class Book {
+  String? title;
+  String? author;
+  int? numberOfpages;
+}
